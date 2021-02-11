b88ec1659a2d85c33eace1c2dd0476630be00569
Jamie Long <jtlong3rd@gmail.com>
2021-02-10 20:59:02 -0500

    Type Short as a function which returns Promise<boolean>, as opposed to a Promise<boolean>

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,9 @@
 const RuleEngine = require('./node-rules');
 
-type Short = Promise<boolean>;
+type Short = (...args: unknown[]) => Promise<boolean>;
+
 type Long = {
-  condition: Function; // examples rules include: isEmployee, isBronzeTier, isInUSACanada
+  condition: (...args: unknown[]) => Promise<boolean>; // examples rules include: isEmployee, isBronzeTier, isInUSACanada
   allow: number; // default 100% allow if func returns true
 };
 
