b192489819020794eb8a2f0e525d513ac21eda0a
Louis Lo <louislo@gmail.com>
2021-03-17 01:22:24 -0700

    Cleanup Rules.ts

--- src/rules.ts
+++ src/rules.ts
@@ -1,32 +1,16 @@
+import type { Result } from './types';
+
 export async function canPassArgumentsToRule(params: {
   userId: string;
-}): Promise<boolean> {
+}): Result {
   const allowList = ['louis'];
-  return Promise.resolve(allowList.includes(params.userId));
+  return allowList.includes(params.userId);
 }
 
-export async function isAnyUser(): Promise<boolean> {
-  return Promise.resolve(true);
+export async function isAnyUser(): Result {
+  return true;
 }
 
-export async function isPastStartTime(params: { startTime: number }): Promise<boolean> {
-  return Promise.resolve(params.startTime <= Date.now());
+export async function isPastStartTime(params: { startTime: number }): Result {
+  return params.startTime <= Date.now();
 }
-
-// async function getGeo() {
-//   return await fetch('https://www.cloudflare.com/cdn-cgi/trace')
-//     .then((response) => response.text())
-//     .then((text) => {
-//       const colo = /^(?:colo)=(.*)$/gm.exec(text)[1];
-//       const loc = /^(?:loc)=(.*)$/gm.exec(text)[1];
-//       return { country: loc, city: colo };
-//     });
-// }
-
-// async function getIP() {
-//   return await fetch('https://www.cloudflare.com/cdn-cgi/trace')
-//     .then((response) => response.text())
-//     .then((text) => {
-//       return /^(?:ip)=(.*)$/gm.exec(text)[1];
-//     });
-// }
